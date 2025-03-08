a5d2f8f1c47ea4e9f16959b7d30dc2f1a3ba9c5e
Misael Borges <ml_bbd@hotmail.com>
2025-03-08 20:41:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import BarraDePesquisa from "./components/BarraDePesquisa";
+import Sidebar from "./components/sidebar";
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <BarraDePesquisa />
+    </div>
+  );
+}
+
+export default App;
